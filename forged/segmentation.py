c6d0a7c2e825e7a9ccd94fdc56ef7ba98a79f8b9
sylvainbonnot <sppbonnot@gmail.com>
2021-10-18 13:35:54 +0200

    added timestamp_annotator

--- forged/segmentation.py
+++ forged/segmentation.py
@@ -1,9 +1,23 @@
 from typing import Callable, Any, Tuple, TypeVar
-from more_itertools import ichunked, split_when, take
+from more_itertools import ichunked, split_when, take, peekable
+from functools import partial
+from lined.tools import BufferStats, Segmenter, return_buffer_on_stats_condition
+from lined import Pipe
+from creek.labeling import DictLabeledElement, ListLabeledElement, SetLabeledElement, label_element
+
+
 
 Timestamp = Any
 Item = TypeVar("Item")
 Callable[[Item], Tuple[Timestamp, Item]]
+
+
+
+def label_stream_on_condition(stream, cond, annotator):
+    for elem in stream:
+        if cond(elem):
+            elem = annotator(elem)
+        yield elem
 
 
 def timestamp_stream(stream, filt, timestamper=enumerate):
@@ -11,6 +25,18 @@ def timestamp_stream(stream, filt, timestamper=enumerate):
         sentinel = filt
         filt = lambda x: x != sentinel
     return filter(lambda x: filt(x[1]), timestamper(stream))
+
+def enumerate_annotation(stream, timestamp_name='timestamp'):
+    stream = peekable(stream)
+    first_item = stream.peek()
+    if not isinstance(first_item, DictLabeledElement):
+        raise NotImplemented('Stream must be of DictLabeledElements')
+    for i, item in enumerate(stream):
+        item = item.add_label({timestamp_name:i})
+        yield item
+
+def timestamp_annotator(stream, timestamper = enumerate_annotation):
+    return timestamper(stream)
 
 
 def segment_equal_lengths(stream, length):
@@ -21,11 +47,19 @@ def segment_equal_lengths(stream, length):
 def segment_on_condition(stream, condition):
     return split_when(stream, condition)
 
+def filter_labelled_list_on_label(stream, label):
+    pass
+
+def filter_labelled_dict_on_label(stream, label, label_value):
+    pass
+
+
+
 
 def segment_on_condition_with_timer(stream, condition, timer):
     substreams = segment_on_condition(stream, condition)  # expect list of substreams
     if isinstance(timer, int):
-        return (take(timer,substream) for substream in substreams)
+        return (take(timer, substream) for substream in substreams)
     else:
         return (filter(timer, substream) for substream in substreams)
 
@@ -36,10 +70,40 @@ def segment_with_lookback(stream, condition, n_lookback):
 
 
 if __name__ == "__main__":
-    condition = lambda x,y : x == 'x'
-    stream = 'abdcxcccdeefxaaassaaxsss'
-    print(stream)
-    timer = 3
-    substreams = segment_on_condition(stream, condition)
-    result = list(segment_on_condition_with_timer(stream, condition, timer))
+    # add docs, notes
+    # place it somewhere else: may be creek instead
+    #gen = "abcxdefghxbbjgjxdddd"
+    """
+    gen = [1,2,3,4,5,6,3,8,9]
+    bs = BufferStats(maxlen=3, func=list)
+    return_if_stats_contains_x = partial(
+        return_buffer_on_stats_condition, cond=lambda x: x!=3, else_val=0
+    )
+
+    def append_annot(annot):
+        def res(item):
+            return "".join(["".join(item), annot])
+
+        return res
+
+    seg = Segmenter(
+        buffer=bs,
+        stats_buffer_callback=lambda x:x,
+    )
+    print(list(map(bs, gen)))
+    #print(list(map(seg, gen)))
+    
+    stream = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    cond = lambda x: x % 5 == 0
+    annotator = partial(label_element, label='divisible by 5', labeled_element_cls=SetLabeledElement)
+    result = label_stream_on_condition(stream, cond, annotator)
     print(list(result))
+    """
+    word = "abcdefg"
+    vals = [i*i for i in range(7)]
+    stream = [DictLabeledElement(val).add_label({'letter': letter}) for val,letter in zip(vals,word)]
+    #result = list(enumerate_annotation(stream))
+    result = timestamp_annotator(stream)
+    result = [item.labels for item in result]
+
+    print(result)
--- forged/tests/segmentation_test.py
+++ forged/tests/segmentation_test.py
@@ -1,9 +1,55 @@
+"""
+Tools to label/annotate stream elements
+
+The motivating example is the case of an incoming stream
+that we need to segment, according to the detection of an event.
+
+For example, take a stream of integers and detect the event "multiple of 5":
+
+```
+1->2->3->4->'multiple of 5'->6->7->...
+
+```
+
+When the stream is "live", we don't want to process it immediately, but instead
+we prefer to annotate it on the fly, by adding some metadata to it.
+
+The simplest addition of metadata information could look like:
+
+```
+3->4->('multiple of 5', 5) -> 6 -> ...
+
+```
+
+This module treats the more complicated case of "multilabelling": a LabelledElement x has an attribute **x.element**,
+and a container of labels **x.labels** (list, set or dict).
+
+Multilabels can be used to segments streams into overlapping segments.
+
+```
+(group0)->(group0)->(group0, group1)->(group0, group1)-> (group1)->(group1)->...
+
+```
+
+"""
+
+
 import pytest
 from forged.segmentation import (
     timestamp_stream,
     segment_equal_lengths,
     segment_on_condition,
     segment_on_condition_with_timer,
+    label_stream_on_condition,
+    enumerate_annotation
+)
+from functools import partial
+from creek.labeling import (
+    DictLabeledElement,
+    ListLabeledElement,
+    SetLabeledElement,
+    LabeledElement,
+    label_element,
 )
 
 
@@ -11,7 +57,55 @@ def test_dummy():
     assert True
 
 
-def test_segment_string():
+def test_label_element():
+    my_label_element = partial(
+        label_element, labeled_element_cls=SetLabeledElement
+    )
+    x = my_label_element(42, "divisible_by_seven")
+    assert (x.element, x.labels) == (42, {"divisible_by_seven"})
+
+
+# may be label everything
+# the more general object would be dictlabelled
+# write on Thoughts on Segmentation
+
+# {'phase':0}: groupby
+# {'start_marker':'start'}
+def test_label_stream_on_condition():
+    stream = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    cond = lambda x: x % 5 == 0
+    annotator = partial(
+        label_element,
+        label="divisible by 5",
+        labeled_element_cls=SetLabeledElement,
+    )
+
+    def view_label(item):
+        if isinstance(item, LabeledElement):
+            return item.labels
+        else:
+            return item
+
+    result = list(
+        map(view_label, label_stream_on_condition(stream, cond, annotator))
+    )
+    expected = [
+        1,
+        2,
+        3,
+        4,
+        {"divisible by 5"},
+        6,
+        7,
+        8,
+        9,
+        {"divisible by 5"},
+        11,
+    ]
+    assert result == expected
+
+
+def test_timestamp_stream():
     stream = "this is a   stream"
     result = list(timestamp_stream(stream, filt=" "))
     expected = [
@@ -54,10 +148,37 @@ def test_segment_on_condition():
     expected = [[1, 2, 3, 3], [2, 5], [2, 4], [2]]
     assert result == expected
 
+
 def test_segment_on_condition_with_timer():
-    condition = lambda x, y: x == 'x'
-    stream = 'abdcxcccdeefxaaassaaxsss'
+    condition = lambda x, y: x == "x"
+    stream = "abdcxcccdeefxaaassaaxsss"
     timer = 3
     result = list(segment_on_condition_with_timer(stream, condition, timer))
-    expected = [['a', 'b', 'd'], ['c', 'c', 'c'], ['a', 'a', 'a'], ['s', 's', 's']]
+    expected = [
+        ["a", "b", "d"],
+        ["c", "c", "c"],
+        ["a", "a", "a"],
+        ["s", "s", "s"],
+    ]
+    assert result == expected
+
+
+def test_enumerate_annotation():
+    word = "abcdefg"
+    vals = [i * i for i in range(7)]
+    stream = [
+        DictLabeledElement(val).add_label({"letter": letter})
+        for val, letter in zip(vals, word)
+    ]
+    result = enumerate_annotation(stream)
+    result = [item.labels for item in result]
+    expected = [
+        {"letter": "a", "timestamp": 0},
+        {"letter": "b", "timestamp": 1},
+        {"letter": "c", "timestamp": 2},
+        {"letter": "d", "timestamp": 3},
+        {"letter": "e", "timestamp": 4},
+        {"letter": "f", "timestamp": 5},
+        {"letter": "g", "timestamp": 6},
+    ]
     assert result == expected
